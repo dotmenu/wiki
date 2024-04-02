d1382fb02a126849779e95851e033e931cea6e0c
Natesworks <149598650+natesworks@users.noreply.github.com>
2024-04-02 17:20:35 +0200

    Prefix https for site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'dotmenu.natesworks.com',
+	site: 'https://dotmenu.natesworks.com',
 	integrations: [
 		starlight({
 			title: 'Dotmenu Wiki',
