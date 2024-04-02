55d6513463904332f8b7d33435ea10624a1fb3aa
Natesworks <natesworks@proton.me>
2024-04-02 17:05:10 +0200

    Add github pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'dotmenu.natesworks.com',
 	integrations: [
 		starlight({
 			title: 'Dotmenu Wiki',
